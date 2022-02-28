20fdc5397714e3e827b54a232cba20a0925c0f10
Terumi <67685581+MySweetBrick@users.noreply.github.com>
2022-02-28 20:10:54 +1300

    Add files via upload
    Navbar done in JS styles

--- Navbar.js
+++ Navbar.js
@@ -1,43 +1,64 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import logo from "../images/Home/Star Logo 07-2.png";
+import React, { useState } from 'react';
+import {
+    NavbarContainer, 
+    LeftContainer, 
+    MiddleContainer,
+    RightContainer,
+    NavbarInnerContainer,
+    NavbarExtendedContainer,
+    NavbarLink,
+    NavbarLinkContainer,
+    Logo,
+    OpenLinksButton,
+    NavbarLinkExtended,
+    LogInLink,
+    LanguageButton,
+    LangContainer,
+} from './Navbar.style';
+import LogoImg from '../images/Home/Star Logo 07-2.png';
+import NZ from '../images/Home/NZ Flag.png';
 import tinoRangatiratanga from '../images/Home/Maori flag.png';
-import nzFlag from '../images/Home/NZ Flag.png';
-import './Navbar.css'
-
 
 
 function Navbar() {
+    const [extendNavbar, setExtendNavbar] = useState(false);
 
-    const [openLinks, setOpenLinks] = useState(false)
-
-    return ( 
-        <div className='navbar'>
-            <div className='navbarLogo'>
-                <Link to='/'> <img src = {logo} alt='' /></Link>
-            </div>
-           <div className='navbarContainer'>
-                <div className='hiddenLinks'>
-                 <button className='button'>
-                 <i className='fa-solid fa-bars'></i>
-                    </button>
-                </div>
-                <Link to='/'>Home</Link> 
-                <Link to='/features'>Features</Link> 
-                <Link to='/teachers'>Teachers</Link> 
-            </div>
-            <div className='signInContainer'> 
-                    <div className='languageButton'>
-                            <img className='nz' src = {nzFlag} alt='' /> 
-                            <img className='rangatiratanga' src = {tinoRangatiratanga} alt=''/> 
-                    </div>
-                    <i className='fa-solid fa-circle-user' />
-                    <div className='signInButton'>
-                        <Link to='signin'>Register/LogIn</Link>
-                    </div>
-                  
-                </div>
-        </div>
-    )
+    return (
+    <NavbarContainer extendNavbar={extendNavbar}>
+        <NavbarInnerContainer>
+        <LeftContainer>
+            <Logo src={LogoImg}></Logo>
+        </LeftContainer>
+        <MiddleContainer>
+            <NavbarLinkContainer>
+            <NavbarLink to='/' >Home</NavbarLink>
+            <NavbarLink to='/features' >Features</NavbarLink>
+            <NavbarLink to='/teachers' >Teachers</NavbarLink>
+            <OpenLinksButton onClick={() => {
+                setExtendNavbar((curr) => !curr);
+            }}
+            >
+            {extendNavbar ? <> &#10005;</> : <> &#8801; </>}
+            </OpenLinksButton>
+            </NavbarLinkContainer>
+        </MiddleContainer>
+        <RightContainer>
+            <LangContainer>LANG
+                <LanguageButton src={NZ} ></LanguageButton>
+                <LanguageButton src={tinoRangatiratanga} ></LanguageButton>
+            </LangContainer>
+            <LogInLink to='/login' >Register/Log In</LogInLink>
+        </RightContainer>
+        </NavbarInnerContainer>
+        {extendNavbar && (
+        <NavbarExtendedContainer>
+            <NavbarLinkExtended to='/' >Home</NavbarLinkExtended>
+            <NavbarLinkExtended to='/features' >Features</NavbarLinkExtended>
+            <NavbarLinkExtended to='/teachers' >Teachers</NavbarLinkExtended>
+        </NavbarExtendedContainer>
+        )}
+        </NavbarContainer>
+    );
 }
-export default Navbar
+
+export default Navbar;
